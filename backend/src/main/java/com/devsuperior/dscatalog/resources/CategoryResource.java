6803817dc428d405b0fa169993bdb2b3074024f0
Luis Fernando Machado <>
2023-08-13 01:09:09 -0300

    layers implementation: Resources, Services, Repositories and Entities

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
+        /* Mock without database access...
         List<Category> listCategory = new ArrayList<>();
 
         listCategory.add(new Category(1L, "Books"));
@@ -21,5 +27,7 @@ public class CategoryResource {
         listCategory.add(new Category(3L, "Mobile"));
 
         return ResponseEntity.ok(listCategory);
+        */
+        return ResponseEntity.ok(this.categoryService.findAll());
     }
 }
